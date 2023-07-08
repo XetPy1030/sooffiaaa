55f06c127e1ec24d02559ad900187163f187aaad
XetPy1030 <xetpy1030@mail.ru>
2023-07-08 15:38:23 +0300

    awydwya

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {Image} from "@mantine/core";
+
+import image from "./img_1.png"
+import {useEffect, useState} from "react";
 
 function App() {
+    const [value, setValue] = useState('');
+    const [isInverted, setIsInverted] = useState(false);
+    const [currentColor, setCurrentColor] = useState('red');
+
+    useEffect(() => {
+        // Функция, которая будет инвертировать изображение по вертикали
+        const invertImage = () => {
+            setIsInverted((prevIsInverted) => !prevIsInverted);
+        };
+
+        // Функция, которая будет менять цвет каждую секунду
+        const changeColor = () => {
+            const colors = ['red', 'green', 'blue'];
+            const randomColor = colors[Math.floor(Math.random() * colors.length)];
+            setCurrentColor(randomColor);
+        };
+
+        // Установка интервала для инвертирования изображения
+        const invertInterval = setInterval(invertImage, 500);
+
+        // Установка интервала для смены цвета
+        const colorInterval = setInterval(changeColor, 500);
+
+        // Очистка интервалов при размонтировании компонента
+        return () => {
+            clearInterval(invertInterval);
+            clearInterval(colorInterval);
+        };
+    }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <img
+              src={image}
+              alt="Your Image"
+              className={`${value} ${isInverted ? 'inverted' : ''}`}
+              style={{ backgroundColor: currentColor }}
+          />
+      </div>
+
   );
 }
 
